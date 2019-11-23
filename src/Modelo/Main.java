0a15b6521a589e8c8a7b7b305304095b6e12d026
GustavoBarahona <58046549+GustavoBarahona@users.noreply.github.com>
2019-11-23 10:47:50 -0500

    Update Main.java

--- src/Modelo/Main.java
+++ src/Modelo/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        mainWindow(stage);
+        mainWindow(stage); //Probando cambio git
     }
 
     /**
